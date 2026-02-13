751a0f41bc9ef7f2bedb0f3f3f2a3de3ceb2e362
igorbasko01 <igorbasko@gmail.com>
2026-02-13 17:17:00 +0200

    add logo and update app name

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Food Locker',
+      title: 'FoodLocker',
       theme: appTheme,
       home: const AppShell(),
     );
