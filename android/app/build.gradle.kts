9d6cd1e4eec291c93b13f6c39203643381752a47
igorbasko01 <igorbasko@gmail.com>
2026-03-14 15:19:04 +0200

    feat: rename android package to com.baskorp.food_locker and update directory structure

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.food_locker"
+    namespace = "com.baskorp.food_locker"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.food_locker"
+        applicationId = "com.baskorp.food_locker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
